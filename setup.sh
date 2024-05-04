44f3863d2ab4a4d6c70a5c30e0e1c88769760be5
sm0317 <m.kurakurakura.s@gmail.com>
2024-05-04 17:10:08 +0900

    bashrc

--- setup.sh
+++ setup.sh
@@ -3,6 +3,7 @@ sudo apt -y update
 # ruby環境構築
 sudo apt-get -y install ruby-full
 curl -fsSL https://github.com/rbenv/rbenv-installer/raw/HEAD/bin/rbenv-installer | bash
+echo 'eval "$(rbenv init -)"' >> ~/.bashrc
 rbenv install 3.1.3
 rbenv global 3.1.3
 rbenv rehash
