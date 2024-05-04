f102252dde62fced96c0a9c534cae6a2e9aff44b
sm0317 <m.kurakurakura.s@gmail.com>
2024-05-04 15:37:44 +0900

    setup更新

--- setup.sh
+++ setup.sh
@@ -1,7 +1,15 @@
 sudo apt update
+
+# ruby環境構築
+sudo apt install rbenv
+rbenv install 3.1.3
+rbenv global 3.1.3
+rbenv rehash
+bundle install
+
+# chromeインストール
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i google-chrome-stable_current_amd64.deb
-sudo apt install sox
-sudo apt install libsox-fmt-mp3
-sudo apt install libsphinxbase3
-bundle install
+
+# soxインストール
+sudo apt install sox libsox-fmt-mp3 libsphinxbase3
