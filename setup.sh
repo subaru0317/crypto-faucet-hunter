fc0c15e97ba7525a128c5cfa4e74761b6885498d
sm0317 <m.kurakurakura.s@gmail.com>
2024-05-04 15:20:35 +0900

    chromeのインストール処理をsetupに追加

--- setup.sh
+++ setup.sh
@@ -1,4 +1,6 @@
 sudo apt update
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome-stable_current_amd64.deb
 sudo apt install sox
 sudo apt install libsox-fmt-mp3
 sudo apt install libsphinxbase3
